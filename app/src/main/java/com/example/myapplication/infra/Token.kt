e45556e52b0213553ce6b7b146472ba72be0db06
HenriqueArgolo <henrique73357@gmail.com>
2024-07-12 13:04:52 -0300

    token

--- app/src/main/java/com/example/myapplication/infra/Token.kt
+++ app/src/main/java/com/example/myapplication/infra/Token.kt
@@ -2,6 +2,6 @@ package com.example.myapplication.infra
 
 object Token {
     object value {
-        const val token ="3600262b8523ce1d9af06c00ccb91257d5b223124f748aee5fc80409107baf92"
+        const val TOKEN ="3600262b8523ce1d9af06c00ccb91257d5b223124f748aee5fc80409107baf92"
     }
 }

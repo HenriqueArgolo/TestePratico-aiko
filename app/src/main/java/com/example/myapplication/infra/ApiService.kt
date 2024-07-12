8aa155c54a9564864767ec1c1edb4410c4d895b3
HenriqueArgolo <henrique73357@gmail.com>
2024-07-12 13:05:00 -0300

    rotas

--- app/src/main/java/com/example/myapplication/infra/ApiService.kt
+++ app/src/main/java/com/example/myapplication/infra/ApiService.kt
@@ -3,24 +3,28 @@ package com.example.myapplication.infra
 import com.example.myapplication.model.BusArriveResponse
 import com.example.myapplication.model.BusStop
 import com.example.myapplication.model.BusStopSearch
-import com.example.myapplication.model.PositionResponse
+import com.example.myapplication.model.Lines
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("Posicao")
-    suspend fun searchBusLine(@Header("Autorzation") token: String):PositionResponse
+
+    @POST("Login/Autenticar")
+    suspend fun auth(@Query("token")token: String): Boolean
+    @GET("Linha/Buscar")
+    suspend fun searchBusLine(@Header("Authorization") token: String, @Query("termosBusca") termoBusca: String):List<Lines>
 
     @GET("Parada/Buscar?termosBusca=")
-    suspend fun getAllBusStop(@Header("Autorzation") token: String): List<BusStop>
+    suspend fun getAllBusStop(): List<BusStop>
 
     @GET("Parada/Buscar")
-    suspend fun getBusStopByNameOrAddress(@Header("Autorzation") token: String, @Query("termosBusca") termosBusca: String): List<BusStopSearch>
+    suspend fun getBusStopByNameOrAddress(@Header("Authorization") token: String, @Query("termosBusca") termosBusca: String): List<BusStopSearch>
 
 
     @GET("Previsao/Parada")
-    suspend fun getBusByBusStopCode(@Header("Autorzation") token: String, @Query("codigoParada") codigoParada: String):BusArriveResponse
+    suspend fun getBusByBusStopCode(@Header("Authorization") token: String, @Query("codigoParada") codigoParada: String):BusArriveResponse
 }
